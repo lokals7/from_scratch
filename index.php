c9430a0e1932bbd539f491c6447605543a9f74c0
Kent Paul <kentpaul777@gmail.com>
2018-08-06 18:37:11 +0800

    1st

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>From Scratch</title>
+	<link rel="stylesheet" href="/style.css">
+</head>
+	<body>
+	</body>
+</html>
