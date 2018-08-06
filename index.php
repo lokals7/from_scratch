65b33cdd271a055e5992719151dc38e3549f8c86
Kent Paul <kentpaul777@gmail.com>
2018-08-06 20:36:38 +0800

    1st

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>From Scratch</title>
-	<link rel="stylesheet" href="/style.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/media.css">
 </head>
 	<body>
+		<h1>Start</h1>
+		<?php
+		echo 'Current PHP version: ' . phpversion();
+		?>
+		<?php phpinfo(); ?>
 	</body>
 </html>
